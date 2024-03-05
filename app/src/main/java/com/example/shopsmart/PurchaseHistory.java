a45b1a4b68af1039f9a36ce7fe0719882499ba79
jamsheeraslam <jamsheeraslam4269@gmail.com>
2024-03-04 20:13:58 -0600

    added purchase history layout with java file

---
+++ app/src/main/java/com/example/shopsmart/PurchaseHistory.java
@@ -0,0 +1,14 @@
+package com.example.shopsmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchaseHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_history);
+    }
+}
